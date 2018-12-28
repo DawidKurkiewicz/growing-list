fc0550412b440e057c0d4c07929fb7c96aa7e957
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2018-12-28 13:40:32 +0100

    elements can be added to list

--- script.js
+++ script.js
@@ -4,12 +4,20 @@ let orderElement = 1;
 const init = () => {
     const btn = document.createElement("button")
     btn.innerHTML = " dodaj 10 elementów do listy"
+    btn.style.fontSize = "30px"
     document.body.appendChild(btn)
     const ul = document.createElement("ul")
     document.body.appendChild(ul)
+    btn.addEventListener("click", createLiElements)
 }
 
 const createLiElements = () => {
+    for (let i = 0; i < 10; i++) {
+        const li = document.createElement("li")
+        li.innerHTML = `element nr ${orderElement++}`
+        document.querySelector("ul").appendChild(li)
+    }
+
 }
 
 init()
