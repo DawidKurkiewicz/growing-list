5ffe656aafeefb95d3c0cc3bdb7b3f5455a77401
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2018-12-28 13:32:39 +0100

    added btn and ul to body

---
+++ script.js
@@ -0,0 +1,15 @@
+let size = 10;
+let orderElement = 1;
+
+const init = () => {
+    const btn = document.createElement("button")
+    btn.innerHTML = " dodaj 10 elementów do listy"
+    document.body.appendChild(btn)
+    const ul = document.createElement("ul")
+    document.body.appendChild(ul)
+}
+
+const createLiElements = () => {
+}
+
+init()
