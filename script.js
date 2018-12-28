9301a4ba38f3427cef1172e8cb30bf35dea72026
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2018-12-28 13:46:13 +0100

    added reset btn

--- script.js
+++ script.js
@@ -3,12 +3,23 @@ let orderElement = 1;
 
 const init = () => {
     const btn = document.createElement("button")
+    const reset = document.createElement("button")
+
     btn.innerHTML = " dodaj 10 elementów do listy"
+    reset.innerHTML = "reset"
+
     btn.style.fontSize = "30px"
+    reset.style.fontSize = "30px"
+
     document.body.appendChild(btn)
+    document.body.appendChild(reset)
+
     const ul = document.createElement("ul")
+    ul.style.listStyle = "none"
     document.body.appendChild(ul)
     btn.addEventListener("click", createLiElements)
+    reset.addEventListener("click", cleanList)
+
 }
 
 const createLiElements = () => {
@@ -18,6 +29,12 @@ const createLiElements = () => {
         li.style.fontSize = `${size++}px`
         document.querySelector("ul").appendChild(li)
     }
+}
+
+const cleanList = () => {
+    document.querySelector("ul").innerHTML = ""
+    size = 10
+    orderElement = 1
 
 }
 
